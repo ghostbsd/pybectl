f03a1ae0369621c9e984b57b69326bc176682b20
ericbsd <ericturgeon.bsd@gmail.com>
2020-07-24 22:10:17 -0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+__VERSION__ = '0.1'
+PROGRAM_VERSION = __VERSION__
+
+setup(
+    name="bectl",
+    version=PROGRAM_VERSION,
+    description="GhostBSD bectl Python module",
+    license='BSD',
+    author='Eric Turgeon',
+    url='https://github/GhostBSD/pybectl/',
+    package_dir={'': '.'},
+    install_requires=['setuptools'],
+    py_modules=['bectl']
+)
